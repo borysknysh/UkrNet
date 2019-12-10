b897bc2198e6b73304ef15e046b8e5117006358b
iodapro <borys.knysh@gmail.com>
2019-12-10 20:06:21 +0100

    ReLU activation

---
+++ include/Activation.h
@@ -0,0 +1,35 @@
+#ifndef ACTIVATION_H
+#define ACTIVATION_H
+
+namespace UkrNet
+{
+    template <class T>
+    class Activation
+    {
+    public:
+        std::vector<T> Forward(const std::vector<T>& input)
+        {
+            std::vector<T> output(input.size(),0);
+            for(auto&& i = 0; i < (int)output.size(); i++)
+            {
+                output[i] = (input[i]<0.)?0.:input[i];
+            }
+            fOutput = output;
+            return std::move(output);
+        }
+        std::vector<T> Backward(const std::vector<T>& inputGradients)
+        {
+            auto&& gradientWrtInput = fOutput;
+            for(auto&& i = 0; i < (int)gradientWrtInput.size(); i++)
+            {
+                auto&& temp = (gradientWrtInput[i] > 0)?1.:0.;
+                gradientWrtInput[i] = temp * inputGradients[i];
+            }
+            
+            return std::move(gradientWrtInput);
+        }
+    private:
+        std::vector<T> fOutput;
+    };
+}
+#endif
