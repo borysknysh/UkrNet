93b103a9472097cd15da65aa4901f045003f7023
Borys Knysh <borys.knysh@gmail.com>
2019-12-10 20:28:52 +0100

    Cmake changed

---
+++ build/CMakeFiles/net2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/net2.dir/net2.cxx.o"
+  "net2.pdb"
+  "net2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/net2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/net2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for net2.
+# This may be replaced when dependencies are built.
